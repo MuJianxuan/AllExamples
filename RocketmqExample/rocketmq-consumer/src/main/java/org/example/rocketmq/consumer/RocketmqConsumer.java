f5fe8ab242305406f331989c430b491d009bcfe5
Rao <1762725704@qq.com>
2022-04-20 16:56:58 +0800

    添加Rocketmq模块

---
+++ RocketmqExample/rocketmq-consumer/src/main/java/org/example/rocketmq/consumer/RocketmqConsumer.java
@@ -0,0 +1,17 @@
+package org.example.rocketmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * desc:
+ *
+ * @author Rao
+ * @Date 2022/04/20
+ **/
+@SpringBootApplication
+public class RocketmqConsumer {
+    public static void main(String[] args) {
+        SpringApplication.run( RocketmqConsumer.class,args);
+    }
+}
---
+++ RocketmqExample/rocketmq-producer/src/main/java/org/example/rocketmq/producer/RocketmqProducer.java
@@ -0,0 +1,17 @@
+package org.example.rocketmq.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * desc:
+ *
+ * @author Rao
+ * @Date 2022/04/20
+ **/
+@SpringBootApplication
+public class RocketmqProducer {
+    public static void main(String[] args) {
+        SpringApplication.run( RocketmqProducer.class,args);
+    }
+}
