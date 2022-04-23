1b2a91998664a439361f9793a58d82d9e933c0ff
rao_m1 <1762725704@qq.com>
2022-04-23 18:41:43 +0800

    添加Rocketmq模式用例

--- RocketmqExample/rocketmq-consumer/src/main/java/org/example/rocketmq/consumer/RocketmqConsumer.java
+++ RocketmqExample/rocketmq-consumer/src/main/java/org/example/rocketmq/consumer/RocketmqConsumer.java
@@ -1,7 +1,16 @@
 package org.example.rocketmq.consumer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * desc:
@@ -14,4 +23,42 @@ public class RocketmqConsumer {
     public static void main(String[] args) {
         SpringApplication.run( RocketmqConsumer.class,args);
     }
+
+    /**
+     * 消费组概念
+     * RocketMQMessageListener 注解的实现是基于 process实现。
+     */
+    @Slf4j
+    @Service
+    @RocketMQMessageListener(topic = "test-topic-1", consumerGroup = "my-consumer_test-topic-1")
+    public static class MyConsumer1 implements RocketMQListener<String> {
+        @Override
+        public void onMessage(String message) {
+            log.info("received message: {}", message);
+        }
+    }
+
+    @Slf4j
+    @Service
+    @RocketMQMessageListener(topic = "test-topic-2", consumerGroup = "my-consumer_test-topic-2")
+    public static class MyConsumer2 implements RocketMQListener<OrderPaidEvent>{
+        @Override
+        public void onMessage(OrderPaidEvent orderPaidEvent) {
+            log.info("received orderPaidEvent: {}", orderPaidEvent);
+        }
+    }
+
+
+    /**
+     * 这里需要留意的是  订单对象的包路径是不同的，看序列化消息接受会不会失败
+     */
+    @Data
+    @AllArgsConstructor
+    public static class OrderPaidEvent implements Serializable {
+
+        private String orderId;
+
+        private BigDecimal paidMoney;
+    }
+
 }
--- RocketmqExample/rocketmq-producer/src/main/java/org/example/rocketmq/producer/RocketmqProducer.java
+++ RocketmqExample/rocketmq-producer/src/main/java/org/example/rocketmq/producer/RocketmqProducer.java
@@ -1,7 +1,18 @@
 package org.example.rocketmq.producer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.support.MessageBuilder;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * desc:
@@ -10,8 +21,49 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2022/04/20
  **/
 @SpringBootApplication
-public class RocketmqProducer {
+public class RocketmqProducer implements CommandLineRunner {
+
+    @Resource
+    private RocketMQTemplate rocketMqTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run( RocketmqProducer.class,args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //send message synchronously 发送简单消息
+        rocketMqTemplate.convertAndSend("test-topic-1", "Hello, World!");
+
+        //send spring message 发送Spring消息
+        rocketMqTemplate.send("test-topic-1", MessageBuilder.withPayload("Hello, World! I'm from spring message").build());
+
+        //send messgae asynchronously 发送异步消息
+        rocketMqTemplate.asyncSend("test-topic-2", new OrderPaidEvent("T_001", new BigDecimal("88.00")), new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.printf("async onSucess SendResult=%s %n", sendResult);
+            }
+
+            @Override
+            public void onException(Throwable var1) {
+                System.out.printf("async onException Throwable=%s %n", var1);
+            }
+
+        });
+
+        //Send messages orderly
+        rocketMqTemplate.syncSendOrderly("orderly_topic",MessageBuilder.withPayload("Hello, World").build(),"hashkey");
+
+        //rocketMqTemplate.destroy(); // notes:  once rocketMqTemplate be destroyed, you can not send any message again with this rocketMqTemplate
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class OrderPaidEvent implements Serializable {
+
+        private String orderId;
+
+        private BigDecimal paidMoney;
     }
 }
