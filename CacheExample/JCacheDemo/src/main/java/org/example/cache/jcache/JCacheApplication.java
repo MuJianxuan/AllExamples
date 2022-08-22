d834cbbe8192c49f76faf296f5dd1824d6c53a22
rao_m1 <1762725704@qq.com>
2022-08-22 21:39:32 +0800

    添加缓存 缓存模块

---
+++ CacheExample/JCacheDemo/src/main/java/org/example/cache/jcache/JCacheApplication.java
@@ -0,0 +1,14 @@
+package org.example.cache.jcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: create 2022/8/21 by rao
+ */
+@SpringBootApplication
+public class JCacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JCacheApplication.class, args);
+    }
+}
--- CacheExample/SpringCacheDemo/src/main/java/org/example/cache/springcache/config/CacheConfiguration.java
+++ CacheExample/SpringCacheDemo/src/main/java/org/example/cache/springcache/config/CacheConfiguration.java
@@ -1,6 +1,12 @@
 package org.example.cache.springcache.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.Cache;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author create 2022/8/17 by rao
@@ -12,9 +18,11 @@ public class CacheConfiguration {
 
     }
 
-    public static void main(String[] args) {
-        //Caffeine<String, User> caffeine = Caffeine.newBuilder();
+    public static final Map<String, Cache> CACHE_MAP = new ConcurrentHashMap<>();
 
+    public static void main(String[] args) {
+        com.github.benmanes.caffeine.cache.Cache<Object, Object> build = Caffeine.newBuilder().build();
+        CACHE_MAP.computeIfAbsent("dd", k -> new CaffeineCache(k,build));
     }
 
 }
