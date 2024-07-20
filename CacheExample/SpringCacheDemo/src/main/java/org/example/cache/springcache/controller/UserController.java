d303440be57237b0fecbec7e01853326f7bd968f
rao <1762725704@qq.com>
2024-07-20 17:00:39 +0800

    优化

--- CacheExample/SpringCacheDemo/src/main/java/org/example/cache/springcache/controller/UserController.java
+++ CacheExample/SpringCacheDemo/src/main/java/org/example/cache/springcache/controller/UserController.java
@@ -15,12 +15,20 @@ public class UserController {
     @Resource
     private UserRepository userRepository;
 
+    /**
+     * 根据用户ID获取用户信息
+     * @param id 用户ID
+     * @return 用户信息
+     */
     @GetMapping("{id}")
     public User getById(@PathVariable("id")Long id){
         return userRepository.getById(id);
     }
 
-
+    /**
+     * 添加新用户
+     * @param user 用户信息
+     */
     @PostMapping
     public void addUser(@RequestBody User user){
         userRepository.add(user);
